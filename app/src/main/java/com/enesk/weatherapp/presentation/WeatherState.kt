6981d4673371d60a0b3015e1958bbf5e46099ae0
kocerenes <eneskocerr41@gmail.com>
2022-09-02 19:58:05 +0300

    Create the viewmodel and screen state

---
+++ app/src/main/java/com/enesk/weatherapp/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.enesk.weatherapp.presentation
+
+import com.enesk.weatherapp.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
