cf4b58d0bb5e5850fa2b3020009ad9425a1aa1b2
kocerenes <eneskocerr41@gmail.com>
2022-09-03 18:36:42 +0300

    Complete the designs

---
+++ app/src/main/java/com/enesk/weatherapp/presentation/ui/WeatherForecast.kt
@@ -0,0 +1,54 @@
+package com.enesk.weatherapp.presentation.ui
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.enesk.weatherapp.presentation.WeatherState
+import com.enesk.weatherapp.presentation.component.HourlyWeatherDisplay
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun WeatherForecast(
+    state: WeatherState,
+    modifier: Modifier = Modifier
+) {
+    state.weatherInfo?.weatherDataPerDay?.get(0)?.let { dataList ->
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                text = "Today",
+                fontSize = 20.sp,
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            LazyRow(content = {
+                items(dataList) { weatherData ->
+                    HourlyWeatherDisplay(
+                        weatherData = weatherData,
+                        modifier = Modifier
+                            .height(100.dp)
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            })
+        }
+    }
+}
+
